1f4899c889167cd2e35e4e88ed1b61e4f008e1c2
ssoyeonni <165532198+ssoyeonni@users.noreply.github.com>
2025-04-09 10:18:24 +0900

    Update Step07CicdApplicationTests.java

--- step07_CICD/src/test/java/edu/fisa/ce/Step07CicdApplicationTests.java
+++ step07_CICD/src/test/java/edu/fisa/ce/Step07CicdApplicationTests.java
@@ -8,6 +8,10 @@ class Step07CicdApplicationTests {
 
 	@Test
 	void contextLoads() {
+		int result = 2+3;
+		System.out.println("비교 전*********");   
+		assertEquals(5, result);
+		System.out.println("비교 후*********");
 	}
 
 }
